d4784c6a731cf7b66dd0136f93edbc4387ad83cf
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-04-07 12:54:56 -0400

    installed and app.js edited

---
+++ dashboard-app-v01/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h2>Dashboard App v1.0</h2>
+      <div>
+        <a>Login</a>
+        <p></p>
+        <a>Register</a>
+      </div>
+    </>
+  );
+}
+
+export default App;
