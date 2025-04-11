955e838a4eebd24cf3db08623a033173fd6c9eea
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-04-11 17:14:04 -0400

    api request added

--- dashboard-app-v03/src/components/Login.js
+++ dashboard-app-v03/src/components/Login.js
@@ -3,7 +3,7 @@ import { useRef, useState } from "react";
 
 
 function Login() {
-    
+
  //link to a page in code - redirect
         let navigate = useNavigate();
 
@@ -21,6 +21,52 @@ function Login() {
         //validate field
 
         //api request
+
+        let _msg = "";
+        let _replit_url = "https://d4a4b615-8c0c-4341-adae-e90a82bcb576-00-2xx2fade5wfsh.worf.replit.dev";
+    
+        const _uid = txtusername.current.value
+        const _pwd = txtpassword.current.value
+        
+        //const _url = `http://localhost:8080/login/${_uid}/${_pwd}`;  
+        const _url = `${_replit_url}/login/${_uid}/${_pwd}`;  
+
+        if(_uid === null || _uid.trim().length === 0)
+        {
+            _msg = "* invalid username";                
+            setMsg(_msg)                
+            return false;
+        }
+        
+        if(_pwd === null || _pwd.trim().length === 0)
+        {
+            _msg = " * invalid password";                
+            setMsg(_msg)                
+            return false;
+        }
+        
+        fetch(_url)
+        .then((res)=>res.json())
+        .then((data)=> {
+            
+            if(data.login === true)
+            {
+                console.log("# navigate to dashbpoard")
+                navigate("/dashboard",{replace:true})
+            }
+
+            setMsg(data.msg)
+        })
+        .catch((error)=>{
+            setMsg("* request error");
+            console.log("* request error *");
+            console.log(error);
+        });
+
+        txtusername.current.value = "";
+        txtpassword.current.value = "";
+
+    
     }
 
     return (
